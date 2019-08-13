b465b4ef76756ed8f5a66b9e0eed42f9efe47029
Michael Sjoeberg <mail@michaelsjoeberg.com>
2019-08-14 01:08:50 +0200

    Initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/* hello.c */
+#include <stdio.h>
+
+/*
+
+    compile     : gcc hello.c -o hello
+    run         : ./hello
+
+*/
+
+int main() {
+    printf ("Hello world!\n");
+
+    return 0;
+}
