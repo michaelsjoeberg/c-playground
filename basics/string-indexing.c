bfdd393fba27453bb6c7552b08757c116a3bea81
Michael Sjoeberg <mail@michaelsjoeberg.com>
2019-08-17 01:13:29 +0200

    Update string-indexing.c

--- basics/string-indexing.c
+++ basics/string-indexing.c
@@ -2,3 +2,9 @@ char string[] = "proton";
 
 string[0]           // p
 string[3]           // t
+
+'''
+Michael Sjoeberg
+2019-08-16
+https://github.com/michaelsjoeberg/c-playground/blob/master/basics/string-indexing.c
+'''
