e5b16c459751fca4080f87c2e4812f4e4d0934a4
Michael Sjoeberg <mail@michaelsjoeberg.com>
2019-08-16 14:51:47 +0200

    constants

---
+++ basics/constants.c
@@ -0,0 +1,13 @@
+// outside scope: define constants
+#define LENGTH 10
+#define WIDTH 5
+
+// inside scope: define constants
+const int LENGTH = 10;
+const int WIDTH = 5;
+
+'''
+Michael Sjoeberg
+2019-08-16
+https://github.com/michaelsjoeberg/c-playground/blob/master/basics/constants.c
+'''
--- basics/test.c
+++ basics/test.c
@@ -2,47 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 
-// declare variable
-//extern int a;
-
-// variable declaration
-extern int x;
-extern char y[];
-extern float z;
-extern double a;            // double size of float
-
 int main() {
-    // define variable
-    //int a;
+    // do something
 
-    // initialise variable
-    //a = 10;
 
-    // print
-    //printf("%d\n", a);
-
-    // variable definition
-    int x;
-
-    // variable definition and initialisation
-    char y[] = "String";
-    float z = 1.05;
-    double a = 12.5;
-
-    // variable initialisation
-    x = 6;
-
-    printf("%d\n", x);
-    printf("%s\n", y);
-    printf("%f\n", z);
-    printf("%f\n", a);
-
-    // multiple assignment
-    int c, d;
-
-    c = d = 6;
-
-    printf("%d, %d\n", c, d);
 
     return 0;
 }
