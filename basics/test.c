3002f5a9ebb54e7f13a3f2cef19eadde1a602016
Michael Sjoeberg <mail@michaelsjoeberg.com>
2019-08-16 13:51:54 +0200

    variables

--- basics/test.c
+++ basics/test.c
@@ -5,6 +5,12 @@
 // declare variable
 //extern int a;
 
+// variable declaration
+extern int x;
+extern char y[];
+extern float z;
+extern double a;            // double size of float
+
 int main() {
     // define variable
     //int a;
@@ -14,6 +20,29 @@ int main() {
 
     // print
     //printf("%d\n", a);
+
+    // variable definition
+    int x;
+
+    // variable definition and initialisation
+    char y[] = "String";
+    float z = 1.05;
+    double a = 12.5;
+
+    // variable initialisation
+    x = 6;
+
+    printf("%d\n", x);
+    printf("%s\n", y);
+    printf("%f\n", z);
+    printf("%f\n", a);
+
+    // multiple assignment
+    int c, d;
+
+    c = d = 6;
+
+    printf("%d, %d\n", c, d);
 
     return 0;
 }
---
+++ basics/variables.c
@@ -0,0 +1,30 @@
+// outside scope
+// variable declaration
+extern int x;
+extern char y[];
+extern float z;
+extern double a;            // double size of float
+
+// inside scope
+// variable definition
+int x;
+
+// variable definition and initialisation
+char y[] = "String";        // String
+float z = 1.05;             // 1.050000
+double a = 12.5;            // 12.500000
+
+// variable initialisation
+x = 6;
+
+// multiple variable definition
+int c, d;
+
+// multiple variable initialisation
+c = d = 6;                  // 6, 6
+
+'''
+Michael Sjoeberg
+2019-08-16
+https://github.com/michaelsjoeberg/c-playground/blob/master/basics/variables.c
+'''
