7153e3f71eb3dbdc153af35bcff5c51ba6060e7e
Michael Sjoeberg <mail@michaelsjoeberg.com>
2019-08-19 23:49:42 +0200

    Create switch.c

---
+++ basics/switch.c
@@ -0,0 +1,21 @@
+int a = 1;
+
+switch (a) {
+    case 0:
+        // do something
+        break;
+    case 1:
+        // do something
+        break;
+    case 2:
+        // do something
+        break;
+    default:
+        // do something
+}
+
+'''
+Michael Sjoeberg
+2019-08-19
+https://github.com/michaelsjoeberg/c-playground/blob/master/basics/switch.c
+'''
