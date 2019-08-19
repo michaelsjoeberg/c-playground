846f2b78c5f1de31eb88aa9a17a20d1246608b66
Michael Sjoeberg <mail@michaelsjoeberg.com>
2019-08-19 23:44:04 +0200

    Create if-else.c

---
+++ basics/if-else.c
@@ -0,0 +1,15 @@
+int a = 1;
+
+if (a < 1) {
+    // do something
+} else if (a == 1) {
+    // do something
+} else {
+    // do something
+}
+
+'''
+Michael Sjoeberg
+2019-08-19
+https://github.com/michaelsjoeberg/c-playground/blob/master/basics/if-else.c
+'''
