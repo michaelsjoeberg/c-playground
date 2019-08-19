7fb8db7815bced36ea3bf6c6cbdf90983c2fe0a0
Michael Sjoeberg <mail@michaelsjoeberg.com>
2019-08-20 00:11:22 +0200

    Create while-loop.c

---
+++ basics/while-loop.c
@@ -0,0 +1,20 @@
+int a = 0;
+
+while (a < 5) {
+    // do something
+
+    a++;
+}
+
+// or
+do {
+    // do something
+
+    a++;
+} while (a < 5);
+
+'''
+Michael Sjoeberg
+2019-08-19
+https://github.com/michaelsjoeberg/c-playground/blob/master/basics/while-loop.c
+'''
