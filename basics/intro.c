6d604eab8557dbe6a7dd7c620638592be58a1949
Michael Sjoeberg <mail@michaelsjoeberg.com>
2019-08-22 14:55:06 +0200

    Update intro.c

--- basics/intro.c
+++ basics/intro.c
@@ -2,6 +2,9 @@
 
 #include <stdio.h>
 
+// compile: $ gcc filename.c -o filename
+// run: $ ./filename
+
 int main(void) {
     // do something
 
@@ -10,8 +13,23 @@ int main(void) {
     return 0;
 }
 
-// compile: $ gcc filename.c -o filename
-// run: $ ./filename
+// command line arguments
+// argc: number of arguments
+// argv: pointer to array with arguments
+int main(int argc, char *argv[]) {
+
+    if (argc == 2) {
+        printf("Argument: %s\n", argv[1]);
+    }
+    else if( argc > 2 ) {
+        printf("%s\n", "Too many arguments.");
+    }
+    else {
+        printf("%s\n", "Expecting one argument.");
+    }
+
+    return 0;
+}
 
 // misc operators
 int a = 10;
