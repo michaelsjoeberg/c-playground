b7089324580d9251754256925cac961a2298a521
Michael Sjoeberg <mail@michaelsjoeberg.com>
2019-08-22 12:52:56 +0200

    Update functions.c

--- basics/functions.c
+++ basics/functions.c
@@ -37,8 +37,37 @@ int power(int base, int x) {
     return result;
 }
 
+// variable arguments
+#include <stdarg.h>
+
+// outside scope: define function with variable arguments
+double average(int n, ...) {
+
+    // requires stdarg.h
+    va_list valist;
+
+    double sum = 0.0;
+    int i;
+
+    // initialise valist for n arguments
+    va_start(valist, n);
+
+    // access arguments assigned to valist
+    for (i = 0; i < n; i++) {
+        sum += va_arg(valist, int);
+    }
+
+    // clean memory reserved for valist
+    va_end(valist);
+
+    return sum / n;
+}
+
+// inside scope: use function
+printf("Average of 2, 3, 4, 5: %f\n", average(2, 3, 4, 5));         // Average of 2, 3, 4, 5: 3.500000
+
 '''
 Michael Sjoeberg
-2019-08-20
+2019-08-22
 https://github.com/michaelsjoeberg/c-playground/blob/master/basics/functions.c
 '''
