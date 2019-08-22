164f5def6f50445b91af2b41919c967dfb7b5b85
Michael Sjoeberg <mail@michaelsjoeberg.com>
2019-08-22 12:44:34 +0200

    Create recursion.c

---
+++ basics/recursion.c
@@ -0,0 +1,24 @@
+// outside scope: define recursive function
+void function() {
+   function();
+}
+
+// example: calculate the factorial
+// outside scope: define recursive function
+unsigned long long int factorial(unsigned int i) {
+    if (i <= 1) {
+        return 1;
+    }
+    return i * factorial(i - 1);
+}
+
+// inside scope: use recursive function
+int i = 12;
+
+printf("Factorial of %d is %llu\n", i, factorial(i));       // Factorial of 12 is 479001600
+
+'''
+Michael Sjoeberg
+2019-08-22
+https://github.com/michaelsjoeberg/c-playground/blob/master/basics/recursion.c
+'''
